3e53c03d5f721e2b16073feabb0f321a512b1c17
Arc <93414133+Grand-Arcanium@users.noreply.github.com>
2024-03-21 20:31:23 -0400

    added config and req

---
+++ config.py
@@ -0,0 +1,16 @@
+"""
+This file contains the bot's token and the way to import it into the program.
+
+@author Mari Shenzhen Uy, Mohawk College, Mar 2024
+@version 1.0
+"""
+
+bot_token = ""
+
+gpt_token = ""
+
+def get_discord_token():
+    return bot_token
+
+def get_gpt_token():
+    return None
